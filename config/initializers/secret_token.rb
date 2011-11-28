46146e003c904f492d3e9d9c9d9adad25c28b6c5
new <new@ubuntu.dlink.com>
2011-11-28 15:08:59 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nihongo::Application.config.secret_token = 'dd6562da52082e514de087aa1c5360dbf4ffac9190c5367de34352632bb5b3418b23ee4091429929e21890eef0166c752820ddce956a2eebb755f8cf44bf5c3d'
---
+++ db/migrate/20111126214929_create_words.rb
@@ -0,0 +1,11 @@
+class CreateWords < ActiveRecord::Migration
+  def change
+    create_table :words do |t|
+      t.string :word
+      t.string :definition
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end
